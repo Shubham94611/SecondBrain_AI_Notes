1a0101d0c87ff341452f2e2a12027486b5397d3d
Shubham Shukla <shubham94611@gmail.com>
2026-05-17 01:00:27 +0530

    Initial Commit

---
+++ SecondBrain_Notes/SecondBrain_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecondBrain_NotesApp.swift
+//  SecondBrain_Notes
+//
+//  Created by Shubham Shukla on 17/05/26.
+//
+
+import SwiftUI
+
+@main
+struct SecondBrain_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
